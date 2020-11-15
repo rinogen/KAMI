96a2c0ba2d3bae60aad757714cd801bc5cb55afc
rinogen <hubertusrino@gmail.com>
2020-11-15 19:44:23 +0700

    Tambah class HasilKalulasiActivity

---
+++ app/src/main/java/com/example/kami/HasilKalulasiActivity.java
@@ -0,0 +1,14 @@
+package com.example.kami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilKalulasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_kalulasi);
+    }
+}
